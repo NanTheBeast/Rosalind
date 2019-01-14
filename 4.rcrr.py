eb79102d88ca59a29f4dcccb1124803a0f5a2ece
NanHu <hunandamon@gmail.com>
2019-01-14 15:45:51 -0800

    Update 4.rcrr.py

--- 4.rcrr.py
+++ 4.rcrr.py
@@ -5,3 +5,31 @@
 # @author: nhu
 
 
+"""
+Usage   Python3 CompeletingDNA.py -h for help.
+        Python3 CompeletingDNA.py -f for filename.
+        The result is 
+"""
+
+
+import argparse
+
+def 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", type=str, help= "input filename")
+    args = parser.parse_args()
+
+    file = args.filename if args.filename is not None else input("input a filename:")
+
+    f = open(file,"r")
+    nt = f.read().strip('\n')
+    f.close()
+
+    tmp = CompleteDNA(nt)
+    # print(tmp)
+    ff = open("3.revc_results.txt","w")
+    ff.write(tmp)
+    ff.close()
